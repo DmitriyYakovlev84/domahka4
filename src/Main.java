fa8de140062d53d4a9f2035408c25ce117a7f45c
Дмитрий <region29@bk.ru>
2023-02-03 14:47:51 +0300

    проверить дз

--- src/Main.java
+++ src/Main.java
@@ -77,7 +77,7 @@ public class Main {
         if (year / 4 == 0 && year / 100 != 0 || year / 400 == 0) {
             System.out.println(year + "является високосным ");
         } else {
-            System.out.println(year + " не является високосным ");
+            System.out.println(year + "  не является високосным ");
         }
     }
 
